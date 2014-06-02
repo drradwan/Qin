c844da3a05458eef797cfac4206bacf54cd0ed96
drradwan <adamradwan@gmail.com>
2014-06-02 11:32:14 -0400

    Re-enable Boshiamy for testing

--- src/QinEngine.cpp
+++ src/QinEngine.cpp
@@ -36,7 +36,7 @@ QinEngine::QinEngine() {
   regInputMethod(new QinTableIMBase(":/data/Latin.xml"));
   //regInputMethod(new QinChewing());
   //regInputMethod(new QinPinyin());
-  //regInputMethod(new QinTableIMBase(":/data/Boshiamy.xml"));
+  regInputMethod(new QinTableIMBase(":/data/Boshiamy.xml"));
   defaultIM = inputMethods[0];
 }
 

2e3a2091ab663cfdf46bd6c1dc3c6cc4598f4d48
drradwan <adamradwan@gmail.com>
2014-06-06 15:20:22 -0400

    Update QinIMBases.cpp

--- src/QinIMBases.cpp
+++ src/QinIMBases.cpp
@@ -394,6 +394,7 @@ void QinTableIMBase::handle_Default(int keyId, bool shifted) {
   if (shifted) {
     bool isOk;
     keyStrokes[keyIndex++] = QString((char) keyId).toUpper().toInt(&isOk, 16);
+    qDebug() << "handle_Default called for Upper case character: " << keyStrokes[keyIndex-1];
   } else { 
     keyStrokes[keyIndex++] = keyId;
   }

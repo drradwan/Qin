187d58ebc233e482770121169441170b2742c98b
drradwan <adamradwan@gmail.com>
2014-06-09 12:06:08 -0400

    Update QinIMBases.cpp

--- src/QinIMBases.cpp
+++ src/QinIMBases.cpp
@@ -280,7 +280,13 @@ bool QinTableIMBase::getDoPopUp(void) {
 }
 
 QStringList QinTableIMBase::getPopUpStrings(void) {
-  return results;
+  if (results.size()) {
+    return results;
+  } else {
+    QStringList numbers;
+    numbers << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "0";
+    return numbers;
+  }
 }
 
 int QinTableIMBase::doQuery() {

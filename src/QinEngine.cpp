8ea41e78de290dfc9358d80b36d9a5d9aeb88f06
drradwan <adamradwan@gmail.com>
2014-06-10 12:11:36 -0400

    Update QinEngine.cpp

--- src/QinEngine.cpp
+++ src/QinEngine.cpp
@@ -163,9 +163,9 @@ void QinEngine::updateHandler(int type) {
       currentIM->reset();
       vkeyboard->show();
       //if (currentIM->getDoPopUp()) {
-        QStringList numbers;
-        numbers << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "0";
-        vkeyboard->showCandStrBar(numbers);
+        QStringList nums;
+        nums << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "0";
+        vkeyboard->showCandStrBar(nums);
       //}
       break;
 

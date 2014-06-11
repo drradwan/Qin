fea3bb815d05c90434a509fdf9063c7fd4bb9cf9
drradwan <adamradwan@gmail.com>
2014-06-11 12:15:59 -0400

    Enable sunpinyin

--- src/QinEngine.cpp
+++ src/QinEngine.cpp
@@ -35,7 +35,7 @@ QinEngine::QinEngine() {
   regInputMethod(new QinIMBase(":/data/Symbols.xml"));
   regInputMethod(new QinTableIMBase(":/data/Latin.xml"));
   //regInputMethod(new QinChewing());
-  //regInputMethod(new QinPinyin());
+  regInputMethod(new QinPinyin());
   regInputMethod(new QinTableIMBase(":/data/Boshiamy.xml"));
   defaultIM = inputMethods[0];
   numbers << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "0";

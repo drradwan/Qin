b8c161d65037c9aa45977e641d943972c2fc36a7
drradwan <adamradwan@gmail.com>
2014-06-11 14:12:54 -0400

    Update QinEngine.cpp

--- src/QinEngine.cpp
+++ src/QinEngine.cpp
@@ -35,8 +35,8 @@ QinEngine::QinEngine() {
   regInputMethod(new QinIMBase(":/data/Symbols.xml"));
   regInputMethod(new QinTableIMBase(":/data/Latin.xml"));
   //regInputMethod(new QinChewing());
-  regInputMethod(new QinPinyin());
-  //regInputMethod(new QinTableIMBase(":/data/Boshiamy.xml"));
+  //regInputMethod(new QinPinyin());
+  regInputMethod(new QinTableIMBase(":/data/Boshiamy.xml"));
   defaultIM = inputMethods[0];
   numbers << "1" << "2" << "3" << "4" << "5" << "6" << "7" << "8" << "9" << "0";
 }

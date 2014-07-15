91bd05de132bff37b2c65631abd42c5ad602903f
drradwan <adamradwan@gmail.com>
2014-07-15 12:05:31 -0400

    Update QVirtualKeyboard.cpp

--- src/QVirtualKeyboard.cpp
+++ src/QVirtualKeyboard.cpp
@@ -202,10 +202,11 @@ void QVirtualKeyboard::s_on_btn_clicked(int btn) {
     //QWSServer::sendKeyEvent(uni, keyId, Modifier, true, false);
     //QWidget *widget = QApplication::focusWidget();
     QKeyEvent key_press(QKeyEvent::KeyPress, keyId, Modifier);
-    QApplication::sendEvent(lineEdit, &key_press);
+    lineEdit->insert(key_press->text());
+    //QApplication::sendEvent(lineEdit, &key_press);
     
-    QKeyEvent key_release(QKeyEvent::KeyRelease, keyId, Modifier);
-    QApplication::sendEvent(lineEdit, &key_release);
+    //QKeyEvent key_release(QKeyEvent::KeyRelease, keyId, Modifier);
+    //QApplication::sendEvent(lineEdit, &key_release);
     
     //lineEdit->insert(QChar(keyId));
   }
@@ -351,10 +352,11 @@ void QVirtualKeyboard::s_on_btnCands_clicked(int btn) {
   //QWSServer::sendKeyEvent(0, keyId, Qt::NoModifier, true, false);
   //QWidget *widget = QApplication::focusWidget();
   QKeyEvent key_press(QKeyEvent::KeyPress, keyId, Qt::NoModifier);
-  QApplication::sendEvent(lineEdit, &key_press);
+  lineEdit->insert(key_press->text());
+  //QApplication::sendEvent(lineEdit, &key_press);
     
-  QKeyEvent key_release(QKeyEvent::KeyRelease, keyId, Qt::NoModifier);
-  QApplication::sendEvent(lineEdit, &key_release);
+  //QKeyEvent key_release(QKeyEvent::KeyRelease, keyId, Qt::NoModifier);
+  //QApplication::sendEvent(lineEdit, &key_release);
   //lineEdit->insert(QChar(keyId));
   clearCandStrBar(true);
 }

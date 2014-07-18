91af96e95005d49840e9c25189121b9864821934
drradwan <adamradwan@gmail.com>
2014-07-18 15:01:51 -0400

    Update QVirtualKeyboard.cpp

--- src/QVirtualKeyboard.cpp
+++ src/QVirtualKeyboard.cpp
@@ -112,6 +112,11 @@ void QVirtualKeyboard::hideAll(void) {
 
 void QVirtualKeyboard::setShift(bool shifted, bool capsed) {
   qDebug() << "QVirtualKeyboard::setShift() called with " << shifted << ", " << capsed;
+  QTimer::singleShot(100, this, setShift2());
+}
+
+void QVirtualKeyboard::setShift2(bool shifted, bool capsed) {
+  qDebug() << "QVirtualKeyboard::setShift2() called with " << shifted << ", " << capsed;
   Shifted = shifted;
   Capsed = capsed;
   Pressed = false;

4a1da83c2ec44a140d0ef78c3cb17d941139471f
drradwan <adamradwan@gmail.com>
2014-07-24 12:19:12 -0400

    Update QinEngine.cpp

--- src/QinEngine.cpp
+++ src/QinEngine.cpp
@@ -176,10 +176,8 @@ void QinEngine::updateHandler(int type) {
       vkeyboard->showCandStrBar(numbers);
       if (currentLanguage.contains("fr") && vkeyboard->isQWERTY) {
         vkeyboard->switchToAZERTY(currentIM);
-        vkeyboard->btnNext->setText("Suivant");
       } else if ((!currentLanguage.contains("fr") && !vkeyboard->isQWERTY) || currentIM->imName.contains("?123")) {
         vkeyboard->switchToQWERTY(currentIM);
-        vkeyboard->btnNext->setText("Next");
       }
       if (vkeyboard->Shifted || vkeyboard->Capsed)
         vkeyboard->changeShiftKeyMap(currentIM);

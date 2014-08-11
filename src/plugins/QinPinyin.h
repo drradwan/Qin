451a0a081c220143962c4f6b623421bd5099c056
drradwan <adamradwan@gmail.com>
2014-08-11 16:39:58 -0400

    Update QinPinyin.h

--- src/plugins/QinPinyin.h
+++ src/plugins/QinPinyin.h
@@ -74,7 +74,7 @@ class QinPinyin: public QinIMBase {
     virtual void reset(void);
 
     /** Key handling APIs **/
-    virtual void handle_Default(int keyId);
+    virtual void handle_Default(int keyId, bool shifted);
     virtual void handle_Space(void);
     virtual void handle_Esc(void);
     virtual void handle_Enter(void);
